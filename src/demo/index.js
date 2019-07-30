ab46c7eb8373f7e978cbf75e3b047d43e0afce13
geckoslair <enrico@piramid.studio>
2019-07-30 17:46:26 +0200

    fixes

--- src/demo/index.js
+++ src/demo/index.js
@@ -8,4 +8,4 @@ var elemGallery = document.getElementById("zot-gallery");
 var zotGallery = new Zot('gallery', elemGallery, {lazyLoad: 1});
 
 var elemAudio = document.getElementById("zot-audio");
-var zotAudio = new Zot('audio', elemAudio, {source: "https://file-examples.com/wp-content/uploads/2017/11/file_example_MP3_5MG.mp3"});
+var zotAudio = new Zot('audio', elemAudio, {source: "https://file-examples.com/wp-content/uploads/2017/11/file_example_MP3_5MG.mp3", audioOnly: true, height: 0});
