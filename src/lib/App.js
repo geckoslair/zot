381a9f5d5d30c18d8042326242260c83e6f5095b
geckoslair <enrico@piramid.studio>
2019-07-30 14:26:56 +0200

    build test

--- src/lib/App.js
+++ src/lib/App.js
@@ -1,8 +1,33 @@
 import './css/App.css';
+import Clappr from 'clappr'
 
 class App {
-  constructor(){
+  constructor(media, elem, options){
+
     console.log("Lib constructor called");
+    this._media = media
+    this._elem = elem
+    this._options = options
+    this.initMedia();
+
+  }
+  initMedia(){
+    switch(this._media){
+
+      case "video":
+        this._player = new Clappr.Player({source: "", parentId: ""});
+      break
+      default:
+      break
+    }
+  }
+
+  get media(){
+    return this._media
+  }
+
+  set media(media){
+    this._media = media
   }
 }
 
--- src/lib/tests/App.test.js
+++ src/lib/tests/App.test.js
@@ -3,3 +3,10 @@ import App from './../App';
 it('renders without crashing', () => {
   new App();
 });
+
+
+it('return media type', () => {
+  const zot = new App('video');
+  expect(zot.media).toBe('video')
+
+});
