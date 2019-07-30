d081c35e424186f39faceb1fdd8d8f5d6e6bb2fe
geckoslair <enrico@piramid.studio>
2019-07-30 17:58:02 +0200

    readme upgrade

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # Zot!
 Zot :cloud_with_lightning: A JS multi media player
+
+## ⭐️ Feature
+
+- Clappr
+- Flickity
+
+## 📦 Getting Started to Develop
+
+```
+git clone https://github.com/geckoslair/zot myLibrary
+yarn
+```
+
+
+## 🚀 How to use it
+
+### Video element
+```
+var elem = document.getElementById("zot-video");
+new Zot('video', elem, {source: "http://techslides.com/demos/sample-videos/small.mp4"});
+```
+
+### Audio element
+```
+var elemGallery = document.getElementById("zot-gallery");
+new Zot('gallery', elemGallery, {lazyLoad: 1});
+```
+
+### Gallery element
+```
+var elemAudio = document.getElementById("zot-audio");
+new Zot('audio', elemAudio, {source: "https://file-examples.com/wp-content/uploads/2017/11/file_example_MP3_5MG.mp3", audioOnly: true});
+```
--- src/demo/index.js
+++ src/demo/index.js
@@ -2,10 +2,10 @@ import Zot from './App';
 
 
 var elem = document.getElementById("zot-video");
-var zotVideo = new Zot('video', elem, {source: "http://techslides.com/demos/sample-videos/small.mp4"});
+new Zot('video', elem, {source: "http://techslides.com/demos/sample-videos/small.mp4"});
 
 var elemGallery = document.getElementById("zot-gallery");
-var zotGallery = new Zot('gallery', elemGallery, {lazyLoad: 1});
+new Zot('gallery', elemGallery, {lazyLoad: 1});
 
 var elemAudio = document.getElementById("zot-audio");
-var zotAudio = new Zot('audio', elemAudio, {source: "https://file-examples.com/wp-content/uploads/2017/11/file_example_MP3_5MG.mp3", audioOnly: true, height: 0});
+new Zot('audio', elemAudio, {source: "https://file-examples.com/wp-content/uploads/2017/11/file_example_MP3_5MG.mp3", audioOnly: true});
